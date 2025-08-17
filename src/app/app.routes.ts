978baff5526da1dbd6039261314010aa596a7b22
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-08-16 18:19:03 -0600

    Implementación de las páginas de Dashboard, Trending y Search, junto con sus respectivas rutas y estilos. Se han añadido pruebas unitarias para cada página. Se eliminó el contenido anterior de app.html y se configuró el enrutamiento para redirigir a la página de Dashboard por defecto.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./gifs/pages/dashboard-page/dashboard-page').then((m) => m.DashboardPage),
+  },
+  {
+    path: 'trending',
+    loadComponent: () =>
+      import('./gifs/pages/trending-page/trending-page').then((m) => m.TrendingPage),
+  },
+  {
+    path: 'search',
+    loadComponent: () =>
+      import('./gifs/pages/search-page/search-page').then((m) => m.SearchPage),
+  },
+  {
+    path: "**",
+    redirectTo: "dashboard"
+  }
+];
